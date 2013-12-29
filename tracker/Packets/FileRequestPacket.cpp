5b9cdbb79b5a4cd0fe172a1bca87f191d69f2204
Benoit <benoit.dumesnil@etu.univ-montp2.fr>
2013-12-30 00:01:41 +0100

    Ajout des FileRequest.

---
+++ tracker/Packets/FileRequestPacket.cpp
@@ -0,0 +1,57 @@
+#include "FileRequestPacket.hpp"
+#include "../Opcode.hpp"
+
+#include <cstring>
+#include <arpa/inet.h>
+#include <stdexcept>
+
+FileRequestPacket::FileRequestPacket(std::string filename, bool send)
+                        : mFileName(filename), mSend(send) {
+}
+
+FileRequestPacket::FileRequestPacket(char* data, int size) {
+    int* opcode = (int*) data;
+    if ((ntohl(*opcode) != getOpcode()) || (size != getSize())) {
+        delete[] data;
+        throw std::runtime_error("Erreur lors du traitement d'un paquet FileRequest\n");
+    }
+
+    char* filename = data + sizeof(getOpcode());
+    mFileName = std::string(filename);
+
+    bool* send = (bool*) (data + sizeof(getOpcode()) + MAX_FILENAME_SIZE);
+    mSend = ntohl(*send);
+
+}
+
+FileRequestPacket::~FileRequestPacket() {
+
+}
+
+int FileRequestPacket::getOpcode() {
+    return FILE_REQUEST;
+}
+
+int FileRequestPacket::getSize() {
+    return sizeof(int) + MAX_FILENAME_SIZE + sizeof(int) + sizeof(int) + sizeof(int);
+}
+
+char* FileRequestPacket::toData() {
+    char* data = new char[getSize()];
+
+    int* opcode = (int*) data;
+    *opcode = htonl(getOpcode());
+
+    char* filename = data + sizeof(getOpcode());
+    strncpy(filename, mFileName.c_str(), MAX_FILENAME_SIZE - 1);
+    filename[MAX_FILENAME_SIZE - 1] = '\0'; // Protection
+
+    int* send = (int*) (data + sizeof(getOpcode()) + MAX_FILENAME_SIZE);
+    *send = htonl(mSend);
+
+    return data;
+}
+
+void FileRequestPacket::exec(std::string adresse) {
+    // TODO
+}
---
+++ tracker/Packets/FileRequestPacket.hpp
@@ -0,0 +1,60 @@
+#ifndef FILE_REQUEST_PACKET_HPP
+#define FILE_REQUEST_PACKET_HPP
+
+#include "IPacket.hpp"
+
+/**
+ * Organisation d'un FileRequest :
+ * -------------------------
+ * |opcode|filename|envoie?|
+ * -------------------------
+ * | 4    | 100    | 1     |
+ * -------------------------
+ * Taille d'un FileRequest : 101 octets
+ */
+class FileRequestPacket : public IPacket {
+    public:
+        static const int MAX_FILENAME_SIZE = 100;
+
+        /**
+         * Construit un paquet à partir de ces paramétres.
+         * @param send true si l'on veux envoyer le fichier.
+         */
+        FileRequestPacket(std::string filename, bool send);
+
+        /**
+         * Reconstruit un paquet à partir des données au format réseau.
+         */
+        FileRequestPacket(char* data, int size);
+
+        ~FileRequestPacket();
+
+        /**
+         * Renvoie l'opcode du packet.
+         */
+        int getOpcode();
+
+        /**
+         * Renvoie la taille du paquet.
+         */
+        int getSize();
+
+        /**
+         * Renvoie le paquet sous la forme d'un tableau de char dans le format réseau.
+         * Note : Les 4 premiers octets sont un entier représentant un opcode.
+         */
+        char* toData();
+
+        /**
+         * Execute la commande associée à ce paquet.
+         * @param adresse L'adresse de la personne ayant envoyé ce paquet.
+         */
+        void exec(std::string adresse);
+
+    private:
+        std::string mFileName;
+        bool mSend;
+};
+
+#endif // IPACKET_HPP
+
